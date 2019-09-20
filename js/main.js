cc4f5c5e4bb7abbe7543935da600e7c95b84e743
Gbahdeyboh <gbahdeybohbello@gmail.com>
2019-09-20 18:00:59 +0100

    deleted server files

--- js/main.js
+++ js/main.js
@@ -7,18 +7,16 @@ const _ouibounce = ouibounce(document.querySelector('#modal'), {
         document.querySelector('#modal-overlay').style.display = 'flex'; // Displays the modal-overlay when users attempt to leave the page
 		// Get the IP address
         const URL = "http://ip-api.com/json/?fields=status,message,country,countryCode,region,regionName,city,zip,lat,lon,timezone,isp,org,as,query";
-        fetch(URL)
-        .then(res => res.json())
-        .then(response => {
+        $.ajax({
+        url: URL,
+        method: "GET"
+        }).then(function(response) {
         	console.log("the response is ", response);
         	let city = response.city;
             let offerText = `WAIT! SPECIAL OFFER FOR ${city.toUpperCase()} RESIDENTS...`
             document.querySelector('#offers').textContent = offerText;
             
-        })
-        .catch(err => {
-        	console.log("Could not fetch the users location");
-        })
+        });
         // The timer
         timer = new FlipClock($('#timer-body'), 3600, {
             clockFace: 'HourlyCounter',
